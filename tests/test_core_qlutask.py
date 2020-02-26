4b7bbe8e17d93fbcf842be6b9de28a1673c40df5
monkut <shane.cousins@gmail.com>
2020-02-26 15:10:47 +0900

    :sparkles: adding `get_scheduled_dates()` method :wrench: adding pre-commit hooks for black/isort

---
+++ tests/test_core_qlutask.py
@@ -0,0 +1,23 @@
+import datetime
+from unittest import TestCase
+
+from qlu.core import QluTask, QluTaskError, QluTaskEstimates
+
+
+class QluTaskTestCase(TestCase):
+    def test_get_scheduled_dates(self):
+        estimates = QluTaskEstimates(minimum=None, suggested=1, maximum=None)
+        qlutask = QluTask(
+            id="taskid-1", absolute_priority=1, estimates=estimates, assignee="sample-user", project_id="proj-1", milestone_id=None, depends_on=None
+        )
+        with self.assertRaises(QluTaskError):
+            qlutask.get_scheduled_dates()
+
+        # add dates
+        sample_date = datetime.date(2020, 1, 1)
+        qlutask.scheduled_dates = [sample_date]
+
+        dates = list(qlutask.get_scheduled_dates())
+        self.assertEqual(dates)
+        self.assertEqual(len(dates), 1)
+        self.assertEqual(dates[0], sample_date)
